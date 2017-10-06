c023211f6952dec0d3eb128b82c5ddc2adc6fa58
shraddha <shraddhabodake22@gmail.com>
2017-10-06 11:53:18 +0530

    fghjkl;

---
+++ IdeaProjects/Demo/src/Prime.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * Created by shraddha on 6/10/17.
+ */
+public class Prime
+{
+    public static void main(String[] args)
+    {
+        int m,num;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the no:");
+        int a=sc.nextInt();
+       m=a/2;
+
+    }
+}
---
+++ IdeaProjects/Demo/src/StrHuyuing.java
@@ -0,0 +1,13 @@
+/**
+ * Created by shraddha on 6/10/17.
+ */
+public class StrHuyuing
+{
+    public static void main(String[] args) {
+        String s="codekul";
+        String p=s.concat("institute");
+        System.out.println(p);
+
+
+    }
+}
