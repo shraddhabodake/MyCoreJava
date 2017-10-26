0af6423067aaf8036582c5ea4bfbf40ac10e8768
shraddha <shraddhabodake22@gmail.com>
2017-10-26 10:12:05 +0530

    Thread

--- IdeaProjects/Demo/src/ThreadDemo.java
+++ IdeaProjects/Demo/src/ThreadDemo.java
@@ -1,5 +1,46 @@
 /**
  * Created by shraddha on 25/10/17.
  */
-public class ThreadDemo {
+public class ThreadDemo extends Thread
+{
+    public void run()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            try
+            {
+
+                Thread.sleep(1000);
+            } catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+
+            System.out.println("codekul");
+        }
+    }
 }
+    class Abc extends Thread {
+        public void run() {
+            for (int i = 0; i < 5; i++) {
+                try {
+
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println("institute");
+            }
+        }
+    }
+
+        class xyz {
+
+            public static void main(String[] args) {
+                ThreadDemo threadDemo = new ThreadDemo();
+                Abc abc=new Abc();
+                threadDemo.start();
+                abc.start();
+            }
+        }
